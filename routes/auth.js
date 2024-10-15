f9c1f41bbd2e04838d0e88027cd379f604bfe581
moury12 <tanzibamouri00@gmail.com>
2024-10-15 22:31:22 +0600

    feat:registration

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const {  generateToken } = require("../middleware/auth_middleware");
 const User = require("../models/user_model");
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
@@ -20,5 +22,35 @@ router.post("/login", async (req, res) => {
         token: generateToken(user)
     })
 });
+router.post('/register',async(req, res)=>{
+const{username, email,password}=req.body;
+try{
+    let user = await User.findOne({email});
+    if(user){
+        return res.status(400).json({ message: 'User already exists' });
 
+    }
+    const hashedPassword = await bcrypt.hash(password,6);
+    user = new User({
+        username,
+        email,
+        password:hashedPassword,
+    });
+    await user.save();
+    const token = jwt.sign({id: user._id,username:user.username}, 'your_jwt_secret');
+    res.json({
+        status: true,
+        message:"User registered successfully",
+        token,
+        user:{
+            id: user._id,
+            username:user.username,
+            email:user.email,
+        }
+    })
+}catch(e){
+    console.log(e);
+    res.status(500).json({status: false, message:'server issue'})
+}
+});
 module.exports = router;
